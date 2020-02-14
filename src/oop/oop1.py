a6026ee1d5b996e763507fd6bfb8ad7216da30b9
“kulkarohan” <kulkarohan@users.noreply.github.com>
2020-02-14 12:31:09 -0500

    initial commit - sprint challenge

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,32 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+# Vehicle is the base class
+class Vehicle:
+    pass
+
+# GroundVehicle is a Vehicle
+class GroundVehicle(Vehicle):
+    pass
+
+# Car is a GroundVehicle
+class Car(GroundVehicle):
+    pass
+
+# Motorcycle is a GroundVehicle
+class Motorcycle(GroundVehicle):
+    pass
+
+# FlightVehicle is a Vehicle
+class FlightVehicle(Vehicle):
+    pass
+
+# Airplane is a FlightVehicle
+class Airplane(FlightVehicle):
+    pass
+
+# Starship is a FlightVehicle
+class Starship(FlightVehicle):
+    pass
